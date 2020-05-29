bb45e9e39d1c3877738eb897aeebf104a07baddb
Jakub Arnold <darthdeus@gmail.com>
2020-05-29 11:22:14 +0200

    Add some styling for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,16 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { setLanguage } from "../features/i18n";
 
+import "./Header.css";
+
 export default function Header() {
   const dispatch = useDispatch();
   return (
-    <div>
-      <button onClick={() => dispatch(setLanguage("en"))}>en</button>
-      <button onClick={() => dispatch(setLanguage("cz"))}>cz</button>
+    <div className="right-header">
+      <div className="languages">
+        <button onClick={() => dispatch(setLanguage("en"))}>en</button>
+        <button onClick={() => dispatch(setLanguage("cz"))}>cz</button>
+      </div>
     </div>
   );
 }

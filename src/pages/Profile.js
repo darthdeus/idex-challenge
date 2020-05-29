46a390b60abededb79d8f8bd637b0fcc3ce30291
Jakub Arnold <darthdeus@gmail.com>
2020-05-29 11:20:23 +0200

    Add at least some info to profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { useI18n } from "../features/i18n";
 
+import profile from "./profile.json";
+import "./profile.css";
+
 export default function Profile() {
   const i18n = useI18n();
+  const user = profile.userAccountInfo;
 
   return (
-    <div>
-      <h1>profile</h1>
-      test
+    <div className="flex profile">
+      <div className="left">
+        <div className="item">Email {user.email}</div>
+
+        <div className="item">Full name: {user.fullName}</div>
+
+        <div className="teams">
+          <ul>
+            {user.teams.map(team => (
+              <li key={team.id}>{team.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="right">Recent likes</div>
     </div>
   );
 }
